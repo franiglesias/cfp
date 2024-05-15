031777fef3149fc3191ad5327638b2a51b44676e
Fran Iglesias <franiglesias@mac.com>
2024-05-15 21:33:03 +0200

    Refactor ReadProposalHandlerTest to use self-shunt stub

--- tests/ForSendProposals/ReadProposal/ReadProposalHandlerTest.php
+++ tests/ForSendProposals/ReadProposal/ReadProposalHandlerTest.php
@@ -7,20 +7,31 @@ namespace App\Tests\ForSendProposals\ReadProposal;
 use App\ForSendProposals\ReadProposal\Proposal;
 use App\ForSendProposals\ReadProposal\ReadProposal;
 use App\ForSendProposals\ReadProposal\ReadProposalHandler;
+use App\ForSendProposals\ReadProposal\ReadProposalResponse;
 use App\ForSendProposals\ReadProposal\RetrieveProposal;
 use PHPUnit\Framework\TestCase;
 use function PHPUnit\Framework\assertEquals;
 
-final class ReadProposalHandlerTest extends TestCase
+final class ReadProposalHandlerTest extends TestCase implements RetrieveProposal
 {
+    private const string PROPOSAL_ID = '01HXE2R5JBCRKAA3K0BZ1TCXT2';
+    private const string NOW = '2024-05-15 21:05';
+
     /** @test */
     public function should_retrieve_proposal_with_id(): void
     {
-        $proposalId = '01HXE2R5JBCRKAA3K0BZ1TCXT2';
-        $now = new \DateTimeImmutable();
+        $handler = new ReadProposalHandler($this);
+        $command = new ReadProposal(self::PROPOSAL_ID);
 
-        $proposal = new Proposal(
-            $proposalId,
+        $response = ($handler)($command);
+
+        $this->assertIsTheExpectedProposal($response);
+    }
+
+    public function __invoke(string $id): Proposal
+    {
+        return new Proposal(
+            self::PROPOSAL_ID,
             'Proposal Title',
             'A description or abstract of the proposal',
             'Fran Iglesias',
@@ -29,27 +40,20 @@ final class ReadProposalHandlerTest extends TestCase
             true,
             'Vigo, Galicia',
             'waiting',
-            $now,
+            new \DateTimeImmutable(self::NOW),
         );
+    }
 
-        $retrieveProposal = $this->createMock(RetrieveProposal::class);
-        $retrieveProposal->method('__invoke')->willReturn($proposal);
-
-        $handler = new ReadProposalHandler($retrieveProposal);
-        $command = new ReadProposal($proposalId);
-
-        $response = ($handler)($command);
-
-        assertEquals($proposalId, $response->id);
+    public function assertIsTheExpectedProposal(ReadProposalResponse $response): void {
+        assertEquals(self::PROPOSAL_ID, $response->id);
         assertEquals('Proposal Title', $response->title);
-        assertEquals('A description or abstract of the proposal',
-            $response->description);
+        assertEquals('A description or abstract of the proposal', $response->description);
         assertEquals('Fran Iglesias', $response->author);
         assertEquals('fran.iglesias@example.com', $response->email);
         assertEquals('talk', $response->type);
         assertEquals(true, $response->sponsored);
         assertEquals('Vigo, Galicia', $response->location);
         assertEquals('waiting', $response->status);
-        assertEquals($now, $response->receivedAt);
+        assertEquals(new \DateTimeImmutable(self::NOW), $response->receivedAt);
     }
 }
