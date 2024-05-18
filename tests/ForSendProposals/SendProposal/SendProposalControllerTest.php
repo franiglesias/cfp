110f131aaaab90e3e9f48d66e6585e37abef9df4
Fran Iglesias <franiglesias@mac.com>
2024-05-18 21:06:46 +0200

    Refactor SendProposalControllerTest to use programmable stubs and fakes

--- tests/ForSendProposals/SendProposal/SendProposalControllerTest.php
+++ tests/ForSendProposals/SendProposal/SendProposalControllerTest.php
@@ -4,10 +4,9 @@ declare (strict_types=1);
 
 namespace App\Tests\ForSendProposals\SendProposal;
 
-use App\ForSendProposals\SendProposal\SendProposal;
+use App\ForSendProposals\SendProposal\ProposalBuilder;
 use App\ForSendProposals\SendProposal\SendProposalController;
 use App\ForSendProposals\SendProposal\SendProposalHandler;
-use App\ForSendProposals\SendProposal\SendProposalResponse;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\Request;
 use function PHPUnit\Framework\assertEquals;
@@ -15,11 +14,28 @@ use function PHPUnit\Framework\assertStringContainsString;
 
 final class SendProposalControllerTest extends TestCase
 {
+    private const string PROPOSAL_TITLE = 'Proposal Title';
+    private const string PROPOSAL_ID = 'proposal-id';
+    private const string NOW = '2024-05-16 12:34:56';
+
+    private SendProposalController $controller;
+
+    protected function setUp(): void
+    {
+        $identityProvider = new IdentityProviderStub(self::PROPOSAL_ID);
+        $clock = new ClockStub(new \DateTimeImmutable(self::NOW));
+
+        $proposalBuilder = new ProposalBuilder($identityProvider, $clock);
+        $storeProposal = new StoreProposalFake();
+
+        $handler = new SendProposalHandler($storeProposal, $proposalBuilder);
+        $this->controller = new SendProposalController($handler);
+    }
+
+
     /** @test */
     public function should_accept_well_formed_proposal(): void
     {
-        $payload = PayloadExample::wellFormedWithTitle('Proposal Title');
-
         $request = Request::create(
             '/api/proposals',
             'POST',
@@ -27,36 +43,14 @@ final class SendProposalControllerTest extends TestCase
             [],
             [],
             ['CONTENT-TYPE' => 'json/application'],
-            json_encode($payload)
-        );
-        $handler = $this->createMock(SendProposalHandler::class);
-
-        $command = new SendProposal(
-            'Proposal Title',
-            'A description or abstract of the proposal',
-            'Fran Iglesias',
-            'fran.iglesias@example.com',
-            'talk',
-            true,
-            'Vigo, Galicia',
+            json_encode(PayloadExample::wellFormedWithTitle(self::PROPOSAL_TITLE))
         );
 
-        $expected = new SendProposalResponse(
-            true,
-            'proposal-id',
-            'Proposal Title'
-        );
-
-        $handler
-            ->method('__invoke')
-            ->with($command)
-            ->willReturn($expected);
-
-        $controller = new SendProposalController($handler);
-        $response = ($controller)($request);
+        $response = ($this->controller)($request);
 
         assertEquals(202, $response->getStatusCode());
-        assertEquals('https://localhost/api/proposals/proposal-id', $response->headers->get("Location"));
+        assertEquals('https://localhost/api/proposals/proposal-id',
+            $response->headers->get("Location"));
 
         $content = json_decode($response->getContent(), true);
         assertStringContainsString(
